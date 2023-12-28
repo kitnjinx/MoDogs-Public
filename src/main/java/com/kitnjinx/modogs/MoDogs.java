87a1992bab4d38d58c831c36f608269bd1f2a17d
kitnjinx <catrinelilypotter@gmail.com>
2023-12-28 09:40:49 -0500

    Base update to 1.19

--- src/main/java/com/kitnjinx/modogs/MoDogs.java
+++ src/main/java/com/kitnjinx/modogs/MoDogs.java
@@ -1,314 +1,38 @@
 package com.kitnjinx.modogs;
 
-import com.kitnjinx.modogs.block.ModBlocks;
-import com.kitnjinx.modogs.entity.ModEntityTypes;
-import com.kitnjinx.modogs.entity.custom.*;
-import com.kitnjinx.modogs.item.ModItems;
-import com.kitnjinx.modogs.villager.ModVillagers;
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.entity.SpawnPlacements;
-import net.minecraft.world.entity.animal.Animal;
-import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-import software.bernie.geckolib3.GeckoLib;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(MoDogs.MOD_ID)
-public class MoDogs
-{
+public class MoDogs {
     public static final String MOD_ID = "modogs";
-
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public MoDogs() {
-        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        ModItems.register(eventBus);
-        ModBlocks.register(eventBus);
+        modEventBus.addListener(this::commonSetup);
 
-        ModVillagers.register(eventBus);
-        ModEntityTypes.register(eventBus);
-
-        eventBus.addListener(this::setup);
-        //eventBus.addListener(this::clientSetup);
-
-        GeckoLib.initialize();
-
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    //clientSetup method moved to ModEventClientBusEvents class
-    /*
-    private void clientSetup(final FMLClientSetupEvent event) {
-        EntityRenderers.register(ModEntityTypes.GERMAN_SHEPHERD.get(), GermanShepherdRenderer::new);
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
     }
-     */
 
-    private void setup(final FMLCommonSetupEvent event) {
-        event.enqueueWork(() -> {
-            ModVillagers.registerPOIs();
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
 
-            SpawnPlacements.register(ModEntityTypes.GERMAN_SHEPHERD.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    Animal::checkAnimalSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.BORDER_COLLIE.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    Animal::checkAnimalSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.GOLDEN_RETRIEVER.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    AbstractDog::checkDogSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.LAB_RETRIEVER.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    AbstractDog::checkDogSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.DACHSHUND.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    Animal::checkAnimalSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.DALMATIAN.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    Animal::checkAnimalSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.CARDIGAN_CORGI.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    Animal::checkAnimalSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.PEMBROKE_CORGI.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    Animal::checkAnimalSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.RUSSELL_TERRIER.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    AbstractDog::checkDogSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.ALASKAN_MALAMUTE.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    AbstractDog::checkDogSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.BERNESE_MOUNTAIN_DOG.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    Animal::checkAnimalSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.SAINT_BERNARD.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    AbstractDog::checkDogSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.BLOODHOUND.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    Animal::checkAnimalSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.BOXER.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    Animal::checkAnimalSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.GREYHOUND.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    AbstractDog::checkDogSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.BOXER.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    Animal::checkAnimalSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.PIT_BULL.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    Animal::checkAnimalSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.GREAT_DANE.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    AbstractDog::checkDogSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.MASTIFF.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    Animal::checkAnimalSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.SHIBA_INU.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    Animal::checkAnimalSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.SHETLAND_SHEEPDOG.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    Animal::checkAnimalSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.BOSTON_TERRIER.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    AbstractDog::checkDogSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.SCOTTISH_TERRIER.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    Animal::checkAnimalSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.CK_CHARLES_SPANIEL.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    AbstractDog::checkDogSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.ITALIAN_GREYHOUND.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    AbstractDog::checkDogSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.AUSTRALIAN_SHEPHERD.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    Animal::checkAnimalSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.BASENJI.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    AbstractDog::checkDogSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.PUG.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    Animal::checkAnimalSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.COCKER_SPANIEL.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    Animal::checkAnimalSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.BULL_TERRIER.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    Animal::checkAnimalSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.MINI_BULL_TERRIER.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    Animal::checkAnimalSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.SCHNAUZER.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    AbstractDog::checkDogSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.MINI_SCHNAUZER.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    AbstractDog::checkDogSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.POODLE.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    AbstractDog::checkDogSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.TOY_POODLE.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    AbstractDog::checkDogSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.DOBERMAN.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    AbstractDog::checkDogSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.MINI_PINSCHER.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    AbstractDog::checkDogSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.HUSKY.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    AbstractDog::checkDogSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.REDBONE_COONHOUND.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    Animal::checkAnimalSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.TREE_WALK_HOUND.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    Animal::checkAnimalSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.AIREDALE_TERRIER.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    Animal::checkAnimalSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.AMERICAN_FOXHOUND.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    Animal::checkAnimalSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.BULLDOG.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    Animal::checkAnimalSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.COLLIE.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    Animal::checkAnimalSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.MUDI.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    AbstractDog::checkDogSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.NORWEGIAN_ELKHOUND.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    AbstractDog::checkDogSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.BEAGLE.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    AbstractDog::checkDogSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.ROTTWEILER.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    AbstractDog::checkDogSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.IRISH_SETTER.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    AbstractDog::checkDogSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.GERMAN_SPITZ.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    AbstractDog::checkDogSpawnRules);
-
-            SpawnPlacements.register(ModEntityTypes.WHIPPET.get(),
-                    SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                    AbstractDog::checkDogSpawnRules);
-        });
+        }
     }
 }
